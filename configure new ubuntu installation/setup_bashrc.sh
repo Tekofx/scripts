79ebc62fb06bb268c34c38477c6860cfacd2aff0
Teko <tekofxx@gmail.com>
2020-10-04 13:24:03 +0200

    Removed variable bash_file

--- configure new ubuntu installation/setup_bashrc.sh
+++ configure new ubuntu installation/setup_bashrc.sh
@@ -1,42 +1,41 @@
 #!/bin/bash
-bash_file="~/.bashrc"
 
 # navi
 echo '
-# navi 
-eval "$(navi widget bash)"'>> bash_file
+# navi
+eval "$(navi widget bash)"'>> ~/.bashrc
 
 
 # editbash
 echo '
 # editbash
-alias editbash="nano ~/.bashrc"' >> bash_file
+alias editbash="nano ~/.bashrc"' >> ~/.bashrc
 
 
-# fzf 
+# fzf
 echo '
 # fzf setup
 export FZF_DEFAULT_OPTS="
 --ansi
 --info=inline
 --height=70%
---layout=reverse' >>bash_file
+--layout=reverse' >>~/.bashrc
 
 echo "--preview-window='right:hidden:wrap:60%'
 --bind='f2:toggle-preview'
 --bind='alt-w:preview-page-up'
 --bind='alt-s:preview-page-down'
---preview 'bat --color=always --style=header,grid --line-range :300 {}'" >> bash_file
-echo '"' >> bash_file
+--preview 'bat --color=always --style=header,grid --line-range :300 {}'" >> ~/.bashrc
+echo '"' >> ~/.bashrc
 
 
 # bat
 echo '
 # bat
-alias bat="batcat"' >> bash_file
+alias bat="batcat"' >> ~/.bashrc
 
 
 # editbash
 echo '
 # editbash
-alias editbash= "nano ~/.bashrc"' >> bash_file
+alias editbash= "nano ~/.bashrc"' >> ~/.bashrc
