03de24c9369cc381215f0c6029dc76b761de1c8e
Teko <tekofxx@gmail.com>
2020-10-25 11:54:10 +0100

    Changed permissions and fixed vim-plu installation on install_neovim.sh

--- configure new ubuntu installation/install_neovim.sh
+++ configure new ubuntu installation/install_neovim.sh
@@ -7,7 +7,8 @@ mkdir ~/.config/nvim
 git clone https://github.com/Tekofx/nvim-config.git ~/.config/nvim
 
 # Install vim-plug
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs \ https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
 # Install all plugins
 nvim +PlugInstall +qa
