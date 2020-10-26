8f6d3a6f890104666c2838baba3c154f6eec381d
Teko <tekofxx@gmail.com>
2020-10-26 15:41:49 +0100

    Fixed zsh script

--- install_zsh.sh
+++ install_zsh.sh
@@ -3,9 +3,6 @@
 # Install zsh
 sudo apt install zsh
 
-# Install Oh-my-zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
 # Install Meslo Font
 wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf ~/.local/share/fonts
 
@@ -14,4 +11,10 @@ git clone https://github.com/romkatv/powerlevel10k.git ~/bin/powerlevel10k
 echo 'source ~/bin/powerlevel10k/powerlevel10k.zsh-theme' >>! ~/.zshrc
 
 # Configure powerlevel10k
+zsh
 p10k configure
+
+# Install Oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+
