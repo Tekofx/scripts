f89b9409d3e193d2276e16e2637688f73bcf223a
Teko <tekofxx@gmail.com>
2020-10-26 20:03:45 +0100

    Added fixme to configure_nvim

--- configure_zsh.sh
+++ configure_zsh.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
 
+# FIXME
+
 # Install Meslo Font
-if [ -f 'MesloLGS NF Regular.ttf' ]; then
-   echo 'The font already exists'
-else
+if [ ! -f '~/.local/share/fonts/MesloLGS NF Regular.ttf' ] 
+then
    wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf ~/.local/share/fonts
+else
+   echo 'The font already exists'
 fi
 
 # Install powerlevel10k
-if [ -f '~/bin/powerlevel10k' ]; then
-   echo 'The powerlevel10k repo already exists'
-else
+if [ ! -d "~/bin/powerlevel10k" ]
+then
    git clone https://github.com/romkatv/powerlevel10k.git ~/bin/powerlevel10k
    echo 'source ~/bin/powerlevel10k/powerlevel10k.zsh-theme' >> ~/.zshrc
+else 
+   echo 'The powerlevel10k repo already exists'
 fi
-
 # Configure powerlevel10k
 source ~/.zshrc
 p10k configure
