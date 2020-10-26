f20d7023780b6c8a58945e548385ec170f8134db
Teko <tekofxx@gmail.com>
2020-10-26 17:05:42 +0100

    Fixed script

--- configure_zsh.sh
+++ configure_zsh.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
 # Install Meslo Font
-wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf ~/.local/share/fonts
+if [ -f 'MesloLGS NF Regular.ttf' ]; then
+   echo 'The font already exists'
+else
+   wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf ~/.local/share/fonts
+fi
 
 # Install powerlevel10k
 git clone https://github.com/romkatv/powerlevel10k.git ~/bin/powerlevel10k
-echo 'source ~/bin/powerlevel10k/powerlevel10k.zsh-theme' >>! ~/.zshrc
+echo 'source ~/bin/powerlevel10k/powerlevel10k.zsh-theme' >> ~/.zshrc
 
 # Configure powerlevel10k
-zsh
+source ~/.zshrc
 p10k configure
 
 # Install Oh-my-zsh
